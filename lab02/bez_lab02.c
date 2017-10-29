9df8611cda4f35ab134b8bbfdf95d0d389de922b
Ponury4ever <wrokiw@o2.pl>
2017-10-29 15:52:04 +0100

    still deadlock but better

--- lab02/bez_lab02.c
+++ lab02/bez_lab02.c
@@ -7,6 +7,7 @@
 #include <stdbool.h>
 
 pthread_mutex_t mutex;
+pthread_cond_t transferCond;
 
 
 typedef struct
@@ -69,12 +70,7 @@ void* addToAccount(void* arg)
 
     for (int i = 0; i < 6; i++)
     {
-
-        if (pthread_mutex_init(&mutex, NULL) != 0)
-        {
-            printf("\n mutex init failed\n");
-            return 0;
-        }
+        pthread_mutex_lock(&mutex);
         while (true)
         {
             if (target->isFree)
@@ -105,81 +101,30 @@ void* addToAccount(void* arg)
 
 void* transferMoney(void* arg)
 {
-    moneyTransfer* transfer = (moneyTransfer*)arg;
+   moneyTransfer* transfer = (moneyTransfer*)arg;
     bankAccount* src = transfer->src;
     bankAccount* target = transfer->target;
 
     for (int i = 0; i < 6; i++)
     {
-
-        if (pthread_mutex_init(&mutex, NULL) != 0)
+        pthread_mutex_lock(&mutex);
+        while (true)
         {
-            printf("\n mutex init failed\n");
-            return 0;
+            if (target->isFree && src->isFree)
+            {
+                target->isFree = false;
+                src->isFree = false;
+                pthread_mutex_unlock(&mutex);
+                break;
+            }
+            else
+            {
+                pthread_cond_wait(&(target->cond), &mutex);
+                pthread_cond_wait(&(src->cond), &mutex);
+                pthread_cond_wait(&(transferCond), &mutex);
+            }
         }
 
-        if (src->id < target->id)
-        {
-
-            while (true)
-            {
-                if (src->isFree)
-                {
-                    src->isFree = false;
-                    pthread_mutex_unlock(&mutex);
-                    break;
-                }
-                else
-                {
-                    pthread_cond_wait(&(src->cond), &mutex);
-                }
-            }
-
-
-            while (true)
-            {
-                if (target->isFree)
-                {
-                    target->isFree = false;
-                    pthread_mutex_unlock(&mutex);
-                    break;
-                }
-                else
-                {
-                    pthread_cond_wait(&(target->cond), &mutex);
-                }
-            }
-        }
-        else
-        {
-            while (true)
-            {
-                if (src->isFree)
-                {
-                    src->isFree = false;
-                    pthread_mutex_unlock(&mutex);
-                    break;
-                }
-                else
-                {
-                    pthread_cond_wait(&(src->cond), &mutex);
-                }
-            }
-
-            while (true)
-            {
-                if (target->isFree)
-                {
-                    target->isFree = false;
-                    pthread_mutex_unlock(&mutex);
-                    break;
-                }
-                else
-                {
-                    pthread_cond_wait(&(target->cond), &mutex);
-                }
-            }
-        }
         int srcCache = src->money;
         int targetCache = target->money;
         usleep(rand() % 1000);
@@ -190,74 +135,77 @@ void* transferMoney(void* arg)
         printf("Do konta nr:  %d --> stan konta: %d \n", target->id, target->money);
 
 
-	pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&mutex);
         src->isFree = true;
-        pthread_cond_signal(&(src->cond));
-        pthread_mutex_unlock(&mutex);	
-   	
-	pthread_mutex_lock(&mutex);
         target->isFree = true;
+        pthread_cond_signal(&(src->cond));
         pthread_cond_signal(&(target->cond));
+        pthread_cond_signal(&(transferCond));
         pthread_mutex_unlock(&mutex);
-        
     }
 }
 
 int main()
 {
-    int i;
+     int i;
     time_t t;
     i = time(&t);
     srand(i);
 
+	if (pthread_mutex_init(&mutex, NULL) != 0)
+   	 {
+        	printf("\n mutex init failed\n");
+       		return 1;
+   	 }
+
     int threadNumber = 5;
 
-    pthread_t addFirstAccountThread[threadNumber];
-    pthread_t addSecondAccountThread[threadNumber];
-    pthread_t transferFirstThread[threadNumber];
-    pthread_t transferSecondThread[threadNumber];
+	pthread_t addFirstAccountThread[threadNumber];
+	pthread_t addSecondAccountThread[threadNumber];
+	pthread_t transferFirstThread[threadNumber];
+	pthread_t transferSecondThread[threadNumber];
 
     int returnThread;
 
-    bankAccount* firstAccount = createAccount(1, 0);
-    bankAccount* secondAccount = createAccount(2, 0);
+	bankAccount *firstAccount = createAccount(1, 0);
+	bankAccount *secondAccount = createAccount(2, 0);
 
-    addMoney* addToFirst = createAdder(firstAccount, 100);
-    addMoney* addToSecond = createAdder(secondAccount, 100);
+	addMoney *addToFirst = createAdder(firstAccount, 100);
+	addMoney *addToSecond = createAdder(secondAccount, 100);
 
-    moneyTransfer* fromFirstTransfer = createTransfer(firstAccount, secondAccount, 50);
-    moneyTransfer* fromSecondTransfer = createTransfer(secondAccount, firstAccount, 150);
+	moneyTransfer *fromFirstTransfer = createTransfer(firstAccount, secondAccount, 50);
+	moneyTransfer *fromSecondTransfer = createTransfer(secondAccount, firstAccount, 150);
 
-    for (int i = 0; i < threadNumber; i++)
+	for (int i = 0; i < threadNumber; i++)
     {
-        pthread_create(&(addFirstAccountThread[i]), NULL, addToAccount, (void*)addToFirst);
+		pthread_create(&(addFirstAccountThread[i]), NULL, addToAccount, (void *)addToFirst);
+		
 
+		pthread_create(&(addSecondAccountThread[i]), NULL, addToAccount, (void *)addToSecond);
+		
 
-        pthread_create(&(addSecondAccountThread[i]), NULL, addToAccount, (void*)addToSecond);
+		pthread_create(&(transferFirstThread[i]), NULL, transferMoney, (void *)fromFirstTransfer);
+		
 
-
-        pthread_create(&(transferFirstThread[i]), NULL, transferMoney, (void*)fromFirstTransfer);
-
-
-        pthread_create(&(transferSecondThread[i]), NULL, transferMoney, (void*)fromSecondTransfer);
+		pthread_create(&(transferSecondThread[i]), NULL, transferMoney, (void *)fromSecondTransfer);
+		
     }
 
-    for (int i = 0; i < threadNumber; i++)
+	for (int i = 0; i < threadNumber; i++)
     {
-        pthread_join(addFirstAccountThread[i], NULL);
+		pthread_join(addFirstAccountThread[i], NULL);
 
-        pthread_join(addSecondAccountThread[i], NULL);
+		pthread_join(addSecondAccountThread[i], NULL);
 
-        pthread_join(transferFirstThread[i], NULL);
+		pthread_join(transferFirstThread[i], NULL);
 
-        pthread_join(transferSecondThread[i], NULL);
+        	pthread_join(transferSecondThread[i], NULL);
     }
 
-
-    printf("\n====================KONIEC====================\n");
-    printf("Konto nr:  %d  --> stan konta: %d \n", firstAccount->id, firstAccount->money);
-    printf("Konto nr:  %d  --> stan konta: %d \n", secondAccount->id, secondAccount->money);
-
+   
+    	printf("\n====================KONIEC====================\n");
+	printf("Konto nr:  %d  --> stan konta: %d \n", firstAccount->id, firstAccount->money);
+	printf("Konto nr:  %d  --> stan konta: %d \n", secondAccount->id, secondAccount->money);
 
     pthread_mutex_destroy(&mutex);
 
